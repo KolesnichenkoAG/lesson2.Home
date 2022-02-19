49cf54894fb75242569902cdf79d38cc6a4ebfff
KolesnichenkoAG <kag.1981@mail.ru>
2022-02-19 15:47:43 +0600

    Initial commit

---
+++ src/main/java/Lesson2Home/MaiApp.java
@@ -0,0 +1,14 @@
+package Lesson2Home;
+
+public class MaiApp {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+        if ((a + b) > 10) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+
+}
