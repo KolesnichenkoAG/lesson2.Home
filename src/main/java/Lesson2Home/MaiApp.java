8969aca9e4e1a858bf8a8423357f9a20b2096ba1
KolesnichenkoAG <kag.1981@mail.ru>
2022-02-19 19:07:50 +0600

    finich

--- src/main/java/Lesson2Home/MaiApp.java
+++ src/main/java/Lesson2Home/MaiApp.java
@@ -3,12 +3,33 @@ package Lesson2Home;
 public class MaiApp {
     public static void main(String[] args) {
         int a = 5;
-        int b = 4;
-        if ((a + b) > 10) {
+        int b = 3;
+        if (a + b > 10 && a + b <= 20) {
             System.out.println("true");
         } else {
             System.out.println("false");
         }
+        isPositiveOrNegative(5);
+        Comparison(3);
+        System.out.println(Comparison(3));
+        printWord("februar", 4);
+        }
+    public static void isPositiveOrNegative(int x){
+        if (x > -1){
+            System.out.println("Число - положительное ");
+        }else{
+            System.out.println("Число отрицательное ");
+        }
     }
-
+    public static boolean Comparison(int x){
+        if (x > -1){
+            return true;
+        } return false;
+    }
+    public static void printWord(String Word, int x){
+        int i = 0;
+        for (; i < x; i ++){
+            System.out.print(Word + "  ");
+        }
+    }
 }
